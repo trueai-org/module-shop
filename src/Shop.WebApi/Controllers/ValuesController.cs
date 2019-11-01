01d7ff3eb2bc97be09acca18c767e426244ce94b
wood <wood@trueai.org>
2019-11-01 12:50:51 +0800

    pro

--- src/Shop.WebApi/Controllers/ValuesController.cs
+++ src/Shop.WebApi/Controllers/ValuesController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Shop.Module.Schedule.Abstractions.Services;
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Shop.WebApi.Controllers
 {
@@ -11,50 +7,33 @@ namespace Shop.WebApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        private readonly IJobService _jobService;
-        private readonly ILogger _logger;
-
-        public ValuesController(IJobService jobService, ILogger<ValuesController> logger)
-        {
-            _jobService = jobService;
-            _logger = logger;
-        }
-
-        // GET api/values
+        // GET: api/Values
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<string>>> Get()
+        public IEnumerable<string> Get()
         {
-            _logger.LogInformation("now:" + DateTime.Now.ToLongTimeString());
-            await _jobService.Schedule(() => Log(), TimeSpan.FromSeconds(5));
             return new string[] { "value1", "value2" };
         }
 
-        public async Task Log()
-        {
-            _logger.LogInformation("after:" + DateTime.Now.ToLongTimeString());
-            await Task.CompletedTask;
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
